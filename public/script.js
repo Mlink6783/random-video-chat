a530c92f43e38976976641052afa7856520e05bc
Mlink6783 <mlink6783@gmail.com>
2025-04-20 15:39:15 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -10,74 +10,66 @@ const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 const status = document.getElementById('status');
 
-// Get user media
+// Get media
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
   .then(stream => {
     localStream = stream;
     localVideo.srcObject = stream;
-
     socket.emit('joinRoom');
-
-    socket.on('waiting', () => {
-      status.innerText = 'Waiting for a partner...';
-    });
-
-    socket.on('matched', async ({ partnerId }) => {
-      status.innerText = 'Matched with a partner!';
-      setupPeerConnection();
-
-      // Must add tracks BEFORE creating offer
-      localStream.getTracks().forEach(track => {
-        peerConnection.addTrack(track, localStream);
-      });
-
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
-      socket.emit('signal', { offer });
-    });
-
-    socket.on('signal', async data => {
-      if (data.offer) {
-        setupPeerConnection();
-
-        localStream.getTracks().forEach(track => {
-          peerConnection.addTrack(track, localStream);
-        });
-
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(answer);
-        socket.emit('signal', { answer });
-      }
-
-      if (data.answer && peerConnection) {
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-      }
-
-      if (data.candidate && peerConnection) {
-        try {
-          await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-        } catch (err) {
-          console.error('Error adding ICE candidate:', err);
-        }
-      }
-    });
-
-    socket.on('partnerDisconnected', () => {
-      status.innerText = 'Partner disconnected. Click "Next" to connect again.';
-      if (peerConnection) {
-        peerConnection.close();
-        peerConnection = null;
-      }
-      remoteVideo.srcObject = null;
-    });
   })
   .catch(err => {
-    console.error('Media device error:', err);
-    alert('Please allow camera and mic permission.');
+    alert('Allow camera and mic!');
+    console.error(err);
   });
 
-function setupPeerConnection() {
+socket.on('waiting', () => {
+  status.innerText = 'Waiting for a partner...';
+});
+
+socket.on('matched', async ({ partnerId }) => {
+  status.innerText = 'Matched!';
+  createPeer();
+
+  localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  socket.emit('signal', { offer });
+});
+
+socket.on('signal', async data => {
+  if (data.offer) {
+    createPeer();
+    localStream.getTracks().forEach(track => {
+      peerConnection.addTrack(track, localStream);
+    });
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit('signal', { answer });
+  }
+
+  if (data.answer) {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+  }
+
+  if (data.candidate) {
+    await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+  }
+});
+
+socket.on('partnerDisconnected', () => {
+  status.innerText = 'Partner left. Click Next to find someone new.';
+  if (peerConnection) {
+    peerConnection.close();
+    peerConnection = null;
+  }
+  remoteVideo.srcObject = null;
+});
+
+function createPeer() {
   if (peerConnection) {
     peerConnection.close();
   }
@@ -91,11 +83,8 @@ function setupPeerConnection() {
   };
 
   peerConnection.ontrack = event => {
-    console.log('Received remote stream');
-    // Multiple tracks may come, check for existing stream
-    if (!remoteVideo.srcObject) {
-      remoteVideo.srcObject = event.streams[0];
-    }
+    console.log('Remote track received');
+    remoteVideo.srcObject = event.streams[0];
   };
 }
 
