c35b278649fa3592b7bf5ea05670b9f388ad386c
Mlink6783 <mlink6783@gmail.com>
2025-04-20 16:28:39 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,112 +1,92 @@
 const socket = io();
 let peerConnection;
 let localStream;
+let remoteStream;
+
+const config = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' }
+  ]
+};
 
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 const status = document.getElementById('status');
 
-// Xirsys ICE servers
-const config = {
-  iceServers: [
-    {
-      urls: "stun:stun.l.google.com:19302"
-    },
-    {
-      urls: "turn:turn.xirsys.com:3478?transport=udp",
-      username: "Adrift1",
-      credential: "78085408-1dcd-11f0-a558-0242ac130003"
-    },
-    {
-      urls: "turn:turn.xirsys.com:3478?transport=tcp",
-      username: "Adrift1",
-      credential: "78085408-1dcd-11f0-a558-0242ac130003"
-    }
-  ]
-};
-window.onload = function() {
-         let xhr = new XMLHttpRequest();
-         xhr.onreadystatechange = function($evt){
-            if(xhr.readyState == 4 && xhr.status == 200){
-                let res = JSON.parse(xhr.responseText);
-                console.log("response: ",res);
-            }
-         }
-         xhr.open("PUT", "https://global.xirsys.net/_turn/MyFirstApp", true);
-         xhr.setRequestHeader ("Authorization": "Basic " + btoa("Adrift1:78085408-1dcd-11f0-a558-0242ac130003") );
-         xhr.setRequestHeader ("Content-Type": "application/json");
-         xhr.send( JSON.stringify({"format": "urls"}) );
-      }:
-
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
   .then(stream => {
     localStream = stream;
     localVideo.srcObject = stream;
-
     socket.emit('joinRoom');
-
-    socket.on('waiting', () => {
-      status.innerText = 'Waiting for a partner...';
-    });
-
-    socket.on('matched', async ({ partnerId }) => {
-      status.innerText = 'Matched with: ' + partnerId;
-      setupPeerConnection();
-      localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
-
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
-      socket.emit('signal', { offer });
-    });
-
-    socket.on('signal', async data => {
-      if (data.offer) {
-        setupPeerConnection();
-        localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(answer);
-        socket.emit('signal', { answer });
-      }
-
-      if (data.answer) {
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-      }
-
-      if (data.candidate) {
-        await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-      }
-    });
-
-    socket.on('partnerDisconnected', () => {
-      status.innerText = 'Your partner has left. Click "Next" to find a new one.';
-      if (peerConnection) {
-        peerConnection.close();
-        peerConnection = null;
-      }
-      remoteVideo.srcObject = null;
-    });
   })
   .catch(error => {
-    console.error('Error accessing media devices:', error);
+    console.error('🎥 Error accessing media devices:', error);
+    status.innerText = 'Media access error.';
   });
 
-function setupPeerConnection() {
+socket.on('waiting', () => {
+  status.innerText = 'Waiting for a partner...';
+});
+
+socket.on('matched', async ({ partnerId }) => {
+  status.innerText = 'Matched with: ' + partnerId;
+  createPeerConnection();
+  localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  socket.emit('signal', { type: 'offer', offer });
+});
+
+socket.on('signal', async data => {
+  if (data.type === 'offer') {
+    createPeerConnection();
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit('signal', { type: 'answer', answer });
+
+  } else if (data.type === 'answer') {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+
+  } else if (data.type === 'candidate') {
+    try {
+      await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+    } catch (e) {
+      console.error('Error adding ICE candidate:', e);
+    }
+  }
+});
+
+socket.on('partnerDisconnected', () => {
+  status.innerText = 'Your partner has left. Click "Next" to find another.';
+  if (peerConnection) {
+    peerConnection.close();
+    peerConnection = null;
+  }
+  remoteVideo.srcObject = null;
+});
+
+function createPeerConnection() {
   if (peerConnection) {
     peerConnection.close();
     peerConnection = null;
   }
 
   peerConnection = new RTCPeerConnection(config);
+  remoteStream = new MediaStream();
+  remoteVideo.srcObject = remoteStream;
 
   peerConnection.onicecandidate = event => {
     if (event.candidate) {
-      socket.emit('signal', { candidate: event.candidate });
+      socket.emit('signal', { type: 'candidate', candidate: event.candidate });
     }
   };
 
   peerConnection.ontrack = event => {
-    remoteVideo.srcObject = event.streams[0];
+    remoteStream.addTrack(event.track);
   };
 }
 
@@ -115,9 +95,7 @@ function connectNextUser() {
     peerConnection.close();
     peerConnection = null;
   }
-
   remoteVideo.srcObject = null;
   status.innerText = 'Connecting to a new user...';
-
   socket.emit('nextUser');
 }
