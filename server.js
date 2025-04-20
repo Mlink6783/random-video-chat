37a34d76f12e868002c168f21322afca1796f9cf
Mlink6783 <mlink6783@gmail.com>
2025-04-20 15:24:40 +0600

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,33 @@ let waitingUser = null;
 io.on('connection', socket => {
   console.log('New user connected:', socket.id);
 
-  if (waitingUser) {
+  socket.on('joinRoom', () => {
+    matchUser(socket);
+  });
+
+  socket.on('nextUser', () => {
+    disconnectPartner(socket);
+    matchUser(socket);
+  });
+
+  socket.on('signal', data => {
+    if (socket.partner) {
+      socket.partner.emit('signal', data);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    disconnectPartner(socket);
+    if (waitingUser === socket) {
+      waitingUser = null;
+    }
+  });
+});
+
+// Match a user with someone else
+function matchUser(socket) {
+  if (waitingUser && waitingUser !== socket) {
     const partner = waitingUser;
     waitingUser = null;
 
@@ -25,26 +51,19 @@ io.on('connection', socket => {
     partner.emit('matched', { partnerId: socket.id });
   } else {
     waitingUser = socket;
+    socket.partner = null;
     socket.emit('waiting');
   }
+}
 
-  socket.on('signal', data => {
-    if (socket.partner) {
-      socket.partner.emit('signal', data);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    if (socket.partner) {
-      socket.partner.emit('partnerDisconnected');
-      socket.partner.partner = null;
-    }
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
-  });
-  
-});
+// Cleanly disconnect a user from their current partner
+function disconnectPartner(socket) {
+  if (socket.partner) {
+    socket.partner.emit('partnerDisconnected');
+    socket.partner.partner = null;
+    socket.partner = null;
+  }
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
